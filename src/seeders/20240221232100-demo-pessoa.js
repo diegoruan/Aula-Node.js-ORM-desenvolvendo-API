f1733fa1cb0106948944de2df81d21d66e3543a5
Diego Ruan <diego_ruan.14@hotmail.com>
2024-02-22 20:43:16 -0300

    Aula 3

--- src/seeders/20240221232100-demo-pessoa.js
+++ src/seeders/20240221232100-demo-pessoa.js
@@ -3,7 +3,6 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    
     await queryInterface.bulkInsert('pessoas', [
       {
         nome: 'Solange Estudante',
